167d71448c7d1a59b0bd410f7deea53f345e121b
Kirt Morgan <kirtmorgan@live.com>
2019-05-01 14:41:29 +0100

    Starting a new project to practice on OOP and SQL in a bigger project

---
+++ boat.py
@@ -0,0 +1,7 @@
+class Boat:
+    def __init__(self, name='', current_location='', destination='', cargo_list=''):
+        self.name = name
+        self.current_location = current_location
+        self.destination = destination
+        self.cargo_list = cargo_list
+        
---
+++ fruit.py
@@ -0,0 +1,4 @@
+class Fruit:
+    def __init__(self, type='', kg=''):
+        self.type_of_fruit = type
+        self.kg_of_fruit = kg
